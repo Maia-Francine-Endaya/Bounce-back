e8af16decc626d1cdfada333d26799c2e56a8fe1
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2023-03-24 10:57:37 +0800

    Added Player Physics

--- Bounce_Back/Assets/Scripts/BallController.cs
+++ Bounce_Back/Assets/Scripts/BallController.cs
@@ -6,30 +6,76 @@ using UnityEngine.SceneManagement;
 public class BallController : MonoBehaviour
 {
   Rigidbody rb;
-  [SerializeField] float movementSpeed = 6f;
+  // [SerializeField] float movementSpeed = 6f;
 
   public int maxHealth = 3;
   public int currentHealth;
 
   public HealthBar health;
+
+  bool shoot;
+  public float shootPower = 10f;
+
+  Vector3 lastpos;
+  Vector3 shootDirection;
   
     void Start()
     {
       rb = GetComponent<Rigidbody>();
+      lastpos = this.transform.position;
+
       currentHealth = maxHealth;
       health.SetMaxHealth(maxHealth);
         
     }
     void Update()
     {
-      float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+      // float horizontalInput = Input.GetAxis("Horizontal");
+      // float verticalInput = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+      // rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        shootDirection = transform.forward + new Vector3(0, 0.25f, 0);
+        shoot = true;
+      }
 
       if (currentHealth == 0) {
         SceneManager.LoadScene(2);
       }
+    }
+
+    void FixedUpdate()
+    {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical"); 
+
+      Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+      rb.AddForce(movement * 10f);
+      if (shoot)
+      {
+        rb.AddForce(shootDirection * shootPower, ForceMode.Impulse);
+        shoot = false;
+      }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacles")
+        {
+            Debug.Log("You've hit a bouncy thing!");
+            // rb.AddForce(-Vector3.forward * 10f, ForceMode.Impulse);
+            rb.AddForce(collision.contacts[0].normal * 10f, ForceMode.Impulse);
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("You've hit a wall!");
+            // rb.AddForce(-Vector3.forward * 5f, ForceMode.Impulse);
+            rb.AddForce(collision.contacts[0].normal * 5f, ForceMode.Impulse);
+        }
     }
 
     void TakeDamage(int damage)
